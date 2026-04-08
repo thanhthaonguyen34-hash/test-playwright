77ce147b8b4f9d798138ff023c0a643d6cf81652
dobuiquy <dobuiquy@gmail.com>
2026-04-08 15:15:55 +0700

    add sort function

--- tests/pages/ProductsPage.ts
+++ tests/pages/ProductsPage.ts
@@ -6,6 +6,10 @@ export class ProductsPage {
   readonly logoutLink: Locator;
   readonly productsTitle: Locator;
   readonly cartIcon: Locator;
+  readonly sortDropdown: Locator;
+  readonly productItems: Locator;
+  readonly productNames: Locator;
+  readonly productPrices: Locator;
   readonly loginUrl = 'https://www.saucedemo.com/';
 
   constructor(page: Page) {
@@ -15,6 +19,10 @@ export class ProductsPage {
     this.logoutLink = page.locator('#logout_sidebar_link');
     this.productsTitle = page.locator('.title');
     this.cartIcon = page.locator('.shopping_cart_link');
+    this.sortDropdown = page.locator('[data-test="product-sort-container"]');
+    this.productItems = page.locator('.inventory_item');
+    this.productNames = page.locator('.inventory_item_name');
+    this.productPrices = page.locator('.inventory_item_price');
   }
 
   async logout() {
@@ -47,5 +55,38 @@ export class ProductsPage {
 
   async addBikeLightToCart() {
     await this.addItemToCart('sauce-labs-bike-light');
+  }
+
+  async sortBy(option: string) {
+    // option can be: 'az' (A to Z), 'za' (Z to A), 'lohi' (Low to High), 'hilo' (High to Low)
+    await this.sortDropdown.selectOption(option);
+    // Wait for sorting to complete
+    await this.page.waitForTimeout(500);
+  }
+
+  async sortByNameAscending() {
+    await this.sortBy('az');
+  }
+
+  async sortByNameDescending() {
+    await this.sortBy('za');
+  }
+
+  async sortByPriceLowToHigh() {
+    await this.sortBy('lohi');
+  }
+
+  async sortByPriceHighToLow() {
+    await this.sortBy('hilo');
+  }
+
+  async getProductNames(): Promise<string[]> {
+    const names = await this.productNames.allTextContents();
+    return names;
+  }
+
+  async getProductPrices(): Promise<number[]> {
+    const prices = await this.productPrices.allTextContents();
+    return prices.map(price => parseFloat(price.replace('$', '')));
   }
 }
